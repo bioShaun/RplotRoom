065a9d6e2e57f53abb1e86d46ab3abe4aa9892a5
Bioshaun <ricekent@163.com>
2018-07-19 16:11:00 +0800

    scatter plot test
    scatter plot test

--- scatter/circ_host_scatter.R
+++ scatter/circ_host_scatter.R
@@ -3,14 +3,15 @@ library(reshape2)
 library(dplyr)
 
 
-circ_prop <- read.delim('./data/circ_host_prop.txt')
-circ_prop$log2_host_tpm <- log2(circ_prop$value_y)
+circ_prop <- read.delim('./data/circ_host_prop2.txt')
+circ_prop$log_host_tpm <- log10(circ_prop$value_y)
+circ_prop$log_circ_tpm <- log10(circ_prop$value_x)
 circ_prop$log2_prop <- log2(circ_prop$host_prop)
 
 select_data <- filter(circ_prop, variable == 'Cerebellum')
-f_circ_prop <- filter(circ_prop, value_x > 1)
+f_circ_prop <- filter(circ_prop, value_x >= 0.5)
 
-p <- ggplot(f_circ_prop, aes(log2_prop, log2_host_tpm, color=variable)) +
+p <- ggplot(circ_prop, aes(log_host_tpm, log_circ_tpm, color=variable)) +
   geom_point() +
   facet_wrap(~variable, nrow = 5)
 p
